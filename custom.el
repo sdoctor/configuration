0549707ff7cf4ee64f245afb39d56a5e7836fbd1
drakeley <sara.drakeley@mobilecoin.com>
2018-08-09 15:37:16 -0700

    Various config changes for mac

--- custom.el
+++ custom.el
@@ -1,7 +1,22 @@
 ;; Make selection highlight as purple
-(set-face-attribute 'helm-selection nil
-                    :background "purple"
-                    :foreground "black")
+;; (set-face-attribute 'helm-selection nil
+;;                     :background "purple"
+;;                     :foreground "black")
+
+;; Sometimes projectile has ^C^P instead of ^C P
+(define-key projectile-mode-map projectile-keymap-prefix nil)
+(define-key projectile-mode-map (kbd "C-c p") #'projectile-command-map)
+
+;; For some reason helm wasn't enabled so I'm enabling it explicitly
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+
+;; For mac emacs standalone
+;; Disable this when using an ergonomic windows keyboard
+;; (setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
 
 ;; For navigating around
 (global-set-key (kbd "M-N") (lambda () (interactive) (next-line 8)))
@@ -23,53 +38,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(company-quickhelp-color-background "#4F4F4F")
- '(company-quickhelp-color-foreground "#DCDCCC")
- '(custom-enabled-themes (quote (wombat)))
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
- '(fci-rule-color "#383838")
- '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (ws-butler auto-complete-exuberant-ctags use-package rg ag dumb-jump matlab-mode modern-cpp-font-lock zop-to-char zenburn-theme which-key volatile-highlights undo-tree switch-window smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters operate-on-number move-text magit key-chord json-mode js2-mode imenu-anywhere ido-completing-read+ hl-todo helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser geben-helm-projectile flycheck flx-ido f expand-region elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel-projectile company browse-kill-ring beacon anzu ace-window)))
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
- '(smartparens-global-mode nil)
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+    (helm yaml-mode dockerfile-mode counsel-projectile rust-mode modern-cpp-font-lock zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens rainbow-mode rainbow-delimiters projectile operate-on-number move-text magit json-mode js2-mode imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck expand-region exec-path-from-shell elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company browse-kill-ring beacon anzu ace-window)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-line ((t (:background "#383838" :height 120 :width normal)))))
 
 ;; For certain config files use ruby-mode
 (add-to-list 'auto-mode-alist '("\\.alg\\'" . ruby-mode))
@@ -82,14 +60,14 @@
 (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
 
 ;; dumb-jump bindings
-(use-package dumb-jump
-             :bind (("M-g o" . dumb-jump-go-other-window)
-                    ("M-g j" . dumb-jump-go)
-                    ("M-g i" . dumb-jump-go-prompt)
-                    ("M-g x" . dumb-jump-go-prefer-external)
-                    ("M-g z" . dumb-jump-go-prefer-external-other-window))
-             :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
-             :ensure)
+;; (use-package dumb-jump
+;;              :bind (("M-g o" . dumb-jump-go-other-window)
+;;                     ("M-g j" . dumb-jump-go)
+;;                     ("M-g i" . dumb-jump-go-prompt)
+;;                     ("M-g x" . dumb-jump-go-prefer-external)
+;;                     ("M-g z" . dumb-jump-go-prefer-external-other-window))
+;;              :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+;;              :ensure)
 
 ;; enable line numbers everywhere
 (global-linum-mode t)
